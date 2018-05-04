16ca43239ca4b879acc1a63c07e79a3d18333c9c
shijinhui11 <1324262694@qq.com>
2018-05-04 16:22:06 +0800

    no message

--- app/src/components/homePage/HomePage.vue
+++ app/src/components/homePage/HomePage.vue
@@ -1,13 +1,26 @@
 <template>
+  <div class="head"></div>
+  <div class="left">
+    <span>当前所在城市:</span>
+  </div>
 
 </template>
 
 <script>
     export default {
         name: "HomePage"
+
     }
 </script>
 
 <style scoped>
 
+  .head{
+    width: 4.6875rem;
+    height:0.571rem;
+    background:#3190e8;
+  }
+  .left span{
+    font-size:.55rem;
+  }
 </style>
